f7b0cd634ec52a61f7719dc1876b803f0a6da54a
avital lis <tali.lisov@gmail.com>
2020-06-11 14:41:09 +0300

    Max's changes

--- HW5/hw5_q1_328982020_315336438.c
+++ HW5/hw5_q1_328982020_315336438.c
@@ -30,32 +30,49 @@ typedef struct hw_component
 
 void check_open_file(FILE *file, char *path, char *file_name);
 
-void read_actions(FILE *actions_file, FILE *components_file);
-int parse_action_line(char *pline, char *action, char *param1, char *param2);
-
-void initialize(HW_component **head_hw_component, FILE *components_file);
-void insert_new_component(HW_component **curr_hw_component, HW_component *new_component);
-void parse_components_line(char *pline, int *copies, char *components);
-int search_and_return_index(char search_term[NAME_LENGTH], HW_component **head_hw_component);
-HW_component** return_pointer(int index, HW_component **head_hw_component);
-void rename_component(char new_name[NAME_LENGTH], HW_component **component_to_change);
-void finalize(HW_component **head_hw_component);
+void procedure_actions(FILE *actions_file, FILE *components_file);
 void call_action(char *action, char *param1, char *param2, HW_component **head_hw_component);
+int parse_action_line(char *pline, char *action, char *param1, char *param2);
+void parse_components_line(char *pline, int *copies, char *components);
 
-int main()
+// utils
+void insert_new_component(HW_component **head_hw_component, HW_component *new_component);
+HW_component* init_new_component(char *name, int copies);
+int search_component_index(char search_term[NAME_LENGTH], HW_component **head_hw_component);
+HW_component** return_pointer_from_component_index(int index, HW_component **head_hw_component);
+
+// supported actions
+void initialize(HW_component **head_hw_component, FILE *components_file);
+void rename_component(char* name, char* new_name, HW_component **head_hw_component);
+void update_component(HW_component **head_hw_component, char *name, char *copies_str);
+void remove_copies_of_component(char* name, char* copies_str, HW_component **head_hw_component);
+void finalize(HW_component **head_hw_component);
+
+
+int main(int argc, char* argv[])
 {
-	char *actions_path = "C:\\Users\\maxg1\\OneDrive\\Documents\\University\\C\\Semester B\\ex5\\txt files\\actions.txt",
-		*components_path = "C:\\Users\\maxg1\\OneDrive\\Documents\\University\\C\\Semester B\\ex5\\txt files\\hw_components.txt",
-		*output_path = "C:\\Users\\maxg1\\OneDrive\\Documents\\University\\C\\Semester B\\ex5\\txt files\\components_updated.txt";  // TODO: turn to args from outside
-	FILE *actions_file = fopen(actions_path, "r"), *components_file = fopen(components_path, "r");;
+	if (argc == 4)
+	{
+		char *components_path = argv[1],
+			*actions_path = argv[2],
+			*output_path = argv[3];  // TODO: turn to args from outside
+		FILE *actions_file = fopen(actions_path, "r"), *components_file = fopen(components_path, "r");;
 
-	check_open_file(actions_file, actions_path, "actions.txt");
-	check_open_file(components_file, components_path, "hw_components.txt");
+		check_open_file(actions_file, actions_path, "actions.txt");
+		check_open_file(components_file, components_path, "hw_components.txt");
 
-	read_actions(actions_file, components_file);
+		procedure_actions(actions_file, components_file);
 
-	fclose(actions_file);
-	fclose(components_file);
+		fclose(actions_file);
+		fclose(components_file);
+		return 0;
+	}
+	else
+	{
+		int number_of_actual_arguments_recieved = argc - 1;
+		printf("Error: invalid number of arguments (<%d> instead of 3)\n", number_of_actual_arguments_recieved);
+		return 1;
+	}
 
 	// TODO: memory leaks check
 
@@ -63,9 +80,9 @@ int main()
 
 void check_open_file(FILE *file, char *path, char *file_name)
 /**
- * Input:
+ * Input: Open file in read mode
  * return parameter: None
- * Function functionality:
+ * Function functionality: Checks if the file was opened successfully
  **/
 {
 	if (file == NULL)
@@ -74,11 +91,11 @@ void check_open_file(FILE *file, char *path, char *file_name)
 	}
 }
 
-void read_actions(FILE *actions_file, FILE *components_file)
+void procedure_actions(FILE *actions_file, FILE *components_file)
 /**
- * Input: open files of the action and the components
+ * Input: Open files of the action and the components
  * return parameter: None
- * Function functionality:
+ * Function functionality: Reads and runs an action per line in the file
  **/
 {
 	char line[ACTION_LINE_LENGTH + 1];
@@ -121,9 +138,9 @@ void read_actions(FILE *actions_file, FILE *components_file)
 
 int parse_action_line(char *pline, char *action, char *param1, char *param2)
 /**
- * Input: line from action_file.txt and pointers to strings
- * return parameter: index to distinguish some of the actions
- * Function functionality: retrieves the action, the component name and the number/component name from the text line
+ * Input: Line from action_file.txt and pointers to strings
+ * return parameter: Index to distinguish some of the actions
+ * Function functionality: Retrieves the action, the component name and the number/component name from the text line
  **/
 {
 	if (!strcmp(pline, "Initialize\n"))
@@ -163,9 +180,9 @@ int parse_action_line(char *pline, char *action, char *param1, char *param2)
 
 void initialize(HW_component **head_hw_component, FILE *components_file)
 /**
- * Input:
+ * Input: Empty head for linked list and the open components file to be read
  * return parameter: None
- * Function functionality:
+ * Function functionality: Creates a sorted linked list from the hardware in the file
  **/
 {
 	char line[COMPONENT_LINE_LENGTH], *component_name = (char*)malloc(sizeof(char)*NAME_LENGTH);
@@ -184,24 +201,22 @@ void initialize(HW_component **head_hw_component, FILE *components_file)
 
 	while (fgets(line, COMPONENT_LINE_LENGTH + 1, components_file) != NULL)
 	{
-		new_component = (HW_component*)malloc(sizeof(HW_component));
-		//current = current->next;
 		component_name = (char*)malloc(sizeof(char)*NAME_LENGTH);
-
 		parse_components_line(line, pcopies, component_name);
-		strcpy(new_component->name, component_name);
-		new_component->copies = copies;
 
+		new_component = init_new_component(component_name, copies);
 		insert_new_component(head_hw_component, new_component);
+
 		free(component_name);
 	}
-	//current->next = NULL;
-	char search_term[NAME_LENGTH] = "Modem";
-	int index = search_and_return_index(search_term, head_hw_component);
-	HW_component** found = return_pointer(index, head_hw_component);
 }
 
 void insert_new_component(HW_component **head_hw_component, HW_component *new_component)
+/**
+ * Input: Head of lined components list and the new component to be added
+ * return parameter: None
+ * Function functionality: Search and find the right place to enter a new component in alphabetic order
+ **/
 {
 	if ((*head_hw_component)->next == NULL)
 	{
@@ -245,6 +260,11 @@ void insert_new_component(HW_component **head_hw_component, HW_component *new_co
 }
 
 void parse_components_line(char *pline, int *copies, char *component)
+/**
+ * Input: Line from hw_components.txt and pointers to strings
+ * return Parameter: None
+ * Function functionality: Retrieves component name and the number of copies
+ **/
 {
 	int line_ix = 0, sub_ix = 0;
 	char *p_current_sub = component, string_copies[10];
@@ -263,10 +283,14 @@ void parse_components_line(char *pline, int *copies, char *component)
 	}
 
 	*copies = atoi(string_copies);
-
 }
 
-int search_and_return_index(char search_term[NAME_LENGTH], HW_component **head_hw_component)
+int search_component_index(char search_term[NAME_LENGTH], HW_component **head_hw_component)
+/**
+ * Input: Name of component to search for, head component in linked list
+ * return parameter: Index of found component in linked list
+ * Function functionality: Searches for a component in linked list. returns index if found, returns "-1" if not found
+ **/
 {
 	int index = 0;
 
@@ -280,7 +304,12 @@ int search_and_return_index(char search_term[NAME_LENGTH], HW_component **head_h
 	return (found != NULL) ? index : -1;
 }
 
-HW_component** return_pointer(int index, HW_component **head_hw_component)
+HW_component** return_pointer_from_component_index(int index, HW_component **head_hw_component)
+/**
+ * Input: Index of component to find, head component in linked list
+ * return parameter: Double pointer to the wanted component
+ * Function functionality: Returns a double pointer to the component in the given index of a linked list
+ **/
 {
 	int counter = 0;
 	HW_component *found = *head_hw_component;
@@ -294,11 +323,6 @@ HW_component** return_pointer(int index, HW_component **head_hw_component)
 	return pfound;
 }
 
-//void rename_component(char new_name[NAME_LENGTH], HW_component **component_to_change)
-//{
-//	(*component_to_change)->name = new_name;
-//}
-
 void finalize(HW_component **head_hw_component)
 /**
  * Input:
@@ -311,37 +335,117 @@ void finalize(HW_component **head_hw_component)
 
 void call_action(char *action, char *param1, char *param2, HW_component **head_hw_component)
 /**
- * Input:
+ * Input: Action name, the first string is the name of the component and the second is either a name or number of copies
  * return parameter: None
- * Function functionality:
+ * Function functionality: Calls supported functions
  **/
 {
 	if (strcmp(action, "Rename") == 0)
 	{
 		{
-			int index = search_and_return_index(param1, head_hw_component);
-			HW_component **component_to_change = return_pointer(index, head_hw_component);
-			//rename_component(param2, component_to_change);
+			rename_component(param1, param2, head_hw_component);
 		}
 	}
+	if (strcmp(action, "Returned_from_customer") == 0 || strcmp(action, "Production") == 0)
+	{
+		update_component(head_hw_component, param1, param2);
+	}
+	if (strcmp(action, "Fatal_malfunction") == 0 || strcmp(action, "Fire") == 0)
+	{
+		remove_copies_of_component(param1, param2, head_hw_component);
+	}
+}
 
+void rename_component(char* name, char* new_name, HW_component **head_hw_component)
+/**
+ * Input: Name to find and change, new name, head component in linked list
+ * return parameter: None
+ * Function functionality: Finds the given component and renames it, then realphabetizes the linked list. If the component isn't found, the function does nothing.
+ **/
+{
 
-	// TODO: add switch case and call the functions according to the text ("fire", "production" etc.)
+	int index = search_component_index(name, head_hw_component);
+
+	if (index == -1)
+	{
+		return;
+	}
+	else if (index == 0)
+	{
+		HW_component** component_to_rename = return_pointer_from_component_index(index, head_hw_component);
+		strcpy((*component_to_rename)->name, new_name);
+		*head_hw_component = (*head_hw_component)->next;
+		insert_new_component(head_hw_component, *component_to_rename);
+	}
+	else
+	{
+		HW_component** component_before_component_to_rename = return_pointer_from_component_index(index - 1, head_hw_component);
+		HW_component* component_to_rename = (*component_before_component_to_rename)->next;
+		strcpy((*component_before_component_to_rename)->next->name, new_name);
+		(*component_before_component_to_rename)->next = (*component_before_component_to_rename)->next->next;
+		insert_new_component(head_hw_component, component_to_rename);
+	}
+}
+
+void update_component(HW_component **head_hw_component, char *name, char *copies_str)
+/**
+ * Input: head component in linked list, the name of the component to change and the copies to be updated
+ * return parameter: None
+ * Function functionality: Updates copies number or add a new component if one does not exist
+ **/
+{
+	int index = search_component_index(name, head_hw_component), copies = atoi(copies_str);
+
+	if (index == -1)
+	{  // TODO: check this segment with different data
+		HW_component *new_component = init_new_component(name, copies);
+		insert_new_component(head_hw_component, new_component);
+	}
+	else
+	{
+		HW_component **returned_hardware = return_pointer_from_component_index(index, head_hw_component);
+		(*returned_hardware)->copies = (*returned_hardware)->copies + copies;
+	}
+}
+
+void remove_copies_of_component(char* name, char* copies_str, HW_component **head_hw_component)
+/**
+ * Input: Name of component to remove copies from, number of copies to be removed, head component of linked list
+ * return parameter: None
+ * Function functionality: Removes the given number of copies from the given component. If the given number of copies is larger than the available copies, the component is updated to 0 copies.
+ **/
+{
+	int index = search_component_index(name, head_hw_component), bad_copies = atoi(copies_str);
+
+	if (index == -1)
+	{
+		return;
+	}
+	else
+	{
+		HW_component** component_to_update = return_pointer_from_component_index(index, head_hw_component);
+		if ((*component_to_update)->copies <= bad_copies)
+		{
+			(*component_to_update)->copies = 0;
+		}
+		else
+		{
+			(*component_to_update)->copies = (*component_to_update)->copies - bad_copies;
+		}
+	}
 }
 
 
-/***
- * Initialize
-Returned_from_customer $$$ Hub ultra $$$ 12
-Fire $$$ Hub $$$ 13
-Returned_from_customer $$$ Modem $$$ 12
-Production $$$ Nic $$$ 2
-Fire $$$ Router promore 10000GG $$$ 1
-Rename $$$ Router pro 100GG $$$ Router pro112 10000GG
-Returned_from_customer $$$ Hub pro $$$ 5
-Production $$$ Nic pro 2000GG $$$ 8
-Fatal_malfunction $$$ Nic 100GG $$$ 7
-Production $$$ Nic pro 2000GG $$$ 8000
-Returned_from_customer $$$ Hub ultraZZZZZ $$$ 12
-Finalize
- */
+
+HW_component* init_new_component(char *name, int copies)
+/**
+ * Input: Component name and number of copies
+ * return parameter: New allocated hw component
+ * Function functionality: Allocates a space for a new component and fills the params
+ **/
+{
+	HW_component* new_component = (HW_component*)malloc(sizeof(HW_component));
+	strcpy(new_component->name, name);
+	new_component->copies = copies;
+	return new_component;
+}
